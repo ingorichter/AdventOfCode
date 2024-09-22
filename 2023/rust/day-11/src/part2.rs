1d361a3f8af9f3aa7f9b3e18a93bf8ad101020a2
Ingo Richter <ingo.richter+github@gmail.com>
2024-09-21 23:27:54 -0700

    part 2 of AOC day 11

--- 2023/rust/day-11/src/part2.rs
+++ 2023/rust/day-11/src/part2.rs
@@ -1,21 +1,53 @@
+use std::char;
+
 use crate::custom_error::AocError;
+use aoc_common::grid::Grid;
+use crate::common::make_grid;
+use crate::common::calc_shortest_path;
+
+pub fn calc(input: &str, factor: usize) -> miette::Result<String, AocError> {
+    let grid: Grid<char> = make_grid(input);
+    let sum: i64 = calc_shortest_path(&grid, factor);
+    Ok(sum.to_string())
+}
 
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    calc(input, 1_000_000)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const TEST_DATA_PART1: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
     #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+    fn test_calc_2() -> miette::Result<()> {
+        assert_eq!("374", calc(TEST_DATA_PART1, 2)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_calc_10() -> miette::Result<()> {
+        assert_eq!("1030", calc(TEST_DATA_PART1, 10)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_calc_100() -> miette::Result<()> {
+        assert_eq!("8410", calc(TEST_DATA_PART1, 100)?);
         Ok(())
     }
 }
