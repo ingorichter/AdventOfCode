7fa9c43c072a8345d67b84eafb6d0663c92833cf
Ingo Richter <ingo.richter+github@gmail.com>
2024-12-08 21:57:09 -0800

    AOC 2024 day 6 part 1

--- 2024/rust/day-06/src/part1.rs
+++ 2024/rust/day-06/src/part1.rs
@@ -1,10 +1,70 @@
-use aoc_common::{common::make_grid, grid::Grid};
+use aoc_common::{
+    common::make_grid,
+    grid::{is_safe, Grid},
+    point::{Point2D, NORTH},
+};
+
+type LabMap = Grid<char>;
+
+const GUARD: char = '^';
+const VISITED: char = 'X';
+const OBSTACLE: char = '#';
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let grid: Grid<char> = make_grid(input);
-    dbg!(&grid);
-    Ok("".to_string())
+    let mut grid = make_grid(input);
+    // agent is always walking north in the beginning
+    let mut guard_position = find_agent_position(&grid);
+    let mut direction = NORTH;
+
+    // iterate as long as the guard is inside the grid
+    while is_safe(&guard_position, &grid) {
+        let new_position = guard_position + direction;
+
+        grid.set(&guard_position, VISITED);
+
+        if grid
+            .get(&new_position)
+            .copied()
+            .is_some_and(|x| x == OBSTACLE)
+        {
+            direction = direction.rotate_clockwise()
+        } else {
+            guard_position = new_position;
+        }
+    }
+
+    let distinct_positions = find_distinct_positions(&grid);
+
+    Ok(distinct_positions.len().to_string())
+}
+
+#[tracing::instrument]
+fn find_distinct_positions(grid: &LabMap) -> Vec<Point2D> {
+    grid.enumerate()
+        .filter_map(|(x, y)| {
+            let x = x as i32;
+            let y = y as i32;
+            let pos = Point2D::new(x, y);
+            if grid.get(&pos).copied() == Some(VISITED) {
+                Some(Point2D::new(x, y))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+#[tracing::instrument]
+fn find_agent_position(grid: &LabMap) -> Point2D {
+    grid.enumerate()
+        .find_map(|(x, y)| {
+            let x = x as i32;
+            let y = y as i32;
+            let pos = Point2D::new(x, y);
+            (grid.get(&pos).copied() == Some(GUARD)).then_some(Point2D::new(x, y))
+        })
+        .unwrap()
 }
 
 #[cfg(test)]
