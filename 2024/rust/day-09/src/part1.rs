30eeab11b8c63abe7ddfd4918627a2990ba56bf6
Ingo Richter <ingo.richter+github@gmail.com>
2024-12-17 11:43:43 +0100

    AOC 2024 day 9 part 1

--- 2024/rust/day-09/src/part1.rs
+++ 2024/rust/day-09/src/part1.rs
@@ -3,12 +3,13 @@ use std::iter::repeat;
 #[derive(Debug, Clone)]
 enum Type {
     Space,
-    File(u32),
+    File(usize),
 }
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let disc_layout: Vec<Type> = input
+        .trim()
         .chars()
         .collect::<Vec<_>>() // Convert to Vec<char> to use chunks
         .chunks(2)
@@ -22,15 +23,58 @@ pub fn process(input: &str) -> miette::Result<String> {
 
             let total_size = pair.0 as usize + pair.1 as usize;
             let mut result = Vec::with_capacity(total_size);
-            result.extend(repeat(Type::File(i.try_into().unwrap())).take(pair.0 as usize));
+            result.extend(repeat(Type::File(i)).take(pair.0 as usize));
             result.extend(repeat(Type::Space).take(pair.1 as usize));
             result
         })
         .collect();
 
-    dbg!(&disc_layout);
+    // dbg!(&disc_layout);
+    // find all indices of empty spaces in reverse order to easier remove them from the list
+    let mut empty_space_indices: Vec<usize> = disc_layout
+        .iter()
+        .enumerate()
+        .filter_map(|(i, t)| if let Type::Space = t { Some(i) } else { None })
+        .rev()
+        .collect();
 
-    Ok("1929".to_string())
+    // dbg!(&empty_space_indices);
+
+    // reverse iterate through disc_layout and move every file to the next free space at the beginning of disc_layout based on the zeroth element of empty_space_indices
+    let checksum: usize = disc_layout
+        .iter()
+        .enumerate()
+        .rev()
+        .filter_map(|(index, t)| match t {
+            Type::File(file_id) => {
+                #[cfg(debug_assertions)]
+                println!("Found file at index {} with id {}", index, file_id);
+
+                if let Some(target_index) = empty_space_indices.pop() {
+                    #[cfg(debug_assertions)]
+                    println!("Moving to target index {}", target_index);
+
+                    Some(file_id * target_index)
+                } else {
+                    Some(file_id * index)
+                }
+            },
+            Type::Space => {
+                #[cfg(debug_assertions)]
+                println!("Found space at index {}", index);
+
+                if !empty_space_indices.is_empty() {
+                    empty_space_indices.remove(0);    
+                }
+                None
+            }
+        })
+        .sum();
+
+    // dbg!(checksum);
+    // dbg!(&disc_layout);
+
+    Ok(checksum.to_string())
 }
 
 #[cfg(test)]
@@ -38,6 +82,7 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn test_process() -> miette::Result<()> {
         let input = "2333133121414131402";
         assert_eq!("1928", process(input)?);
@@ -47,7 +92,7 @@ mod tests {
     #[test]
     fn test_process_simple() -> miette::Result<()> {
         let input = "12345";
-        assert_eq!("1928", process(input)?);
+        assert_eq!("61", process(input)?);
         Ok(())
     }
 }
